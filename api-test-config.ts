104ad444896e2b88b2b952bb047eb8496c8bb234
Gytis Norutis <gytis.norutis@outlook.com>
2026-02-08 16:27:25 +0000

    Enhance api-test-config to ensure env variables are set

--- api-test-config.ts
+++ api-test-config.ts
@@ -1,7 +1,11 @@
 const config = {
-    apiUrl:  'https://conduit-api.bondaracademy.com/api',
-    userEmail: process.env.USER_EMAIL,
-    userPassword: process.env.USER_PASSWORD
+    apiUrl: 'https://conduit-api.bondaracademy.com/api',
+    userEmail: process.env.USER_EMAIL || throwError('USER_EMAIL required'),
+    userPassword: process.env.USER_PASSWORD || throwError('USER_PASSWORD required')
 }
 
-export { config }
+function throwError(msg: string): never {
+    throw new Error(msg);
+}
+
+export { config };
